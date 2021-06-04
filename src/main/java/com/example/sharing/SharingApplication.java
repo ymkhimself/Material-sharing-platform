58d52cec68f8265a4b35903da5c34d42ba2490cd
ymk <1766273199@qq.com>
2021-06-04 22:19:26 +0800

    项目刚创建

---
+++ src/main/java/com/example/sharing/SharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingApplication.class, args);
+    }
+
+}
