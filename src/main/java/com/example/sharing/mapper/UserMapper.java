3266f58a60ecea37ae619e757bb3a733b8524bbd
ymk <1766273199@qq.com>
2021-06-07 15:26:30 +0800

    1

---
+++ src/main/java/com/example/sharing/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.sharing.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.sharing.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
